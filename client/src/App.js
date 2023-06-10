cce80ab4f2f5db921ff613d50fad9465575b6c4a
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-06-10 17:06:54 +0200

    routes part

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import './stylesheets/allignements.css';
+import './stylesheets/custom.css';
+import './stylesheets/sizes.css';
+import './stylesheets/theme.css';
+import './stylesheets/form-elements.css';
+
+
+function App() {
+  return (
+    <div
+    className="bg-primary flex justify-center items-center h-screen"
+    >
+      <h1>
+      Library Management System
+      </h1>
+
+    </div>
+
+
+
+  );
+}
+
+export default App;
