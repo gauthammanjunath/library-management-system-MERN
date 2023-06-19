1552eaf16b8d94164546e1e66df8d06217c97f0a
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-06-19 23:50:23 +0200

    Done with Register

--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { Form } from "antd";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="h-screen  bg-primary">
+      <div className="authentication-form bg-white p-2">
+        <Form layout="vertical">
+          <Form.Item
+            label="Name"
+            name="email"
+          >
+            < input type="text" placeholder="Name" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+          >
+            < input type="email" placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            label="Confirm Password"
+            name="Confirm Password"
+          >
+            < input type="password" placeholder="Password" />
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register 
