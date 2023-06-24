7cdf559166c0f4a057b0e97ca243d1e19cb7b8ea
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-06-24 16:39:41 +0200

    done with login and register ui

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from 'react';
+import { Form } from "antd";
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
+
 
 function Login() {
+
+  const onFinish = (values) => {
+    console.log("success:", values);
+  };
   return (
-    <div>Login</div>
-  )
+    <div className="h-screen  bg-primary flex items-center justify-center">
+      <div className="authentication-form bg-white p-3">
+        <h1 className="text-primary text-2xl font-bold mb-1"><Link to="https://www.srh-hochschule-heidelberg.de/landingpage/dein-studium-im-bereich-nachhaltigkeitsmanagement-in-heidelberg/?gclid=Cj0KCQjwqNqkBhDlARIsAFaxvwyvQBmaYZpnKVAiPZSNKTnx0hjK6ukXlpMw2zUbbl6czPDY14mcTVIaAgpcEALw_wcB">SRH Hochschule Library </Link></h1>
+        <hr />
+        <Form layout="vertical"
+          onFinish={onFinish} className="mt-1">
+          <Form.Item
+            label="Email"
+            name="Email"
+          >
+            < input type="email" placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            name="Password"
+          >
+            < input type="password" placeholder="Password" />
+          </Form.Item>
+
+          <div className="text-center  mt-2 flex flex-col gap-1">
+            <Button title='Login' type="Submit" />
+            <Link to="/Register"
+              className='text-primary text-sm underline'
+            >Don't have an account?Click here to Register</Link>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Register/index.js
+++ client/src/pages/Register/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form } from "antd";
 import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
 
 
 function Register() {
@@ -10,11 +11,11 @@ function Register() {
   };
   return (
     <div className="h-screen  bg-primary flex items-center justify-center">
-      <div className="authentication-form bg-white p-2">
-       <h1 className="text-Primary text-2xl">SRH Library-Register</h1>
-
+      <div className="authentication-form bg-white p-3">
+       <h1 className="text-primary text-2xl font-bold mb-1"><Link to="https://www.srh-hochschule-heidelberg.de/landingpage/dein-studium-im-bereich-nachhaltigkeitsmanagement-in-heidelberg/?gclid=Cj0KCQjwqNqkBhDlARIsAFaxvwyvQBmaYZpnKVAiPZSNKTnx0hjK6ukXlpMw2zUbbl6czPDY14mcTVIaAgpcEALw_wcB">SRH Hochschule Library </Link></h1>
+        <hr/>
         <Form layout="vertical"
-          onFinish={onFinish}
+          onFinish={onFinish} className="mt-1"
         >
           <Form.Item
             label="Name"
@@ -34,7 +35,13 @@ function Register() {
           >
             < input type="password" placeholder="Password" />
           </Form.Item>
+       
+          <div className="text-center  mt-2 flex flex-col gap-1">
           <Button title='Register' type="Submit" />
+            <Link to="/Login"
+            className='text-primary text-sm underline'
+            >Already have an account?Click here to login</Link>
+          </div>
         </Form>
       </div>
     </div>
