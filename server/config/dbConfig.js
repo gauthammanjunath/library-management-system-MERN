9072da38c953ced5ef95115e0ac3d1d4a8ed965e
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-06-26 01:09:18 +0200

    try to fix mongo

---
+++ server/config/dbConfig.js
@@ -0,0 +1,12 @@
+const mongoose =require("mongoose");
+mongoose.set('strictQuery',false)
+mongoose.connect(process.env.mongo_url)
+
+const connection = mongoose.connection;
+connection.on('connected',()=> {
+    console.log("Mongo DB COnnection Successfull");
+})
+connection.on('error',()=> {
+    console.log("Mongo DB COnnection Failed");
+})
+module.exports = connection;
