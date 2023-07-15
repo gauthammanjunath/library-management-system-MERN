c0e2e19fde8b5a04870210547a7697c25ba3eacb
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-07-15 19:09:37 +0200

    error

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Tabs } from 'antd';
+import Books from './Books';
+import Users from './Users';
+const TabPane =Tabs.TabPane;
+
 
 function Profile() {
   return (
-    <div>Profile</div>
-  )
+    <div>
+      <Tabs defaultActiveKey='1'>
+        <TabPane tab='Books' key='1'>
+          <Books />
+        </TabPane>
+      <TabPane tab='Users' key='2'>
+        <Users />
+      </TabPane>
+    </Tabs>
+   </div>
+  );
 }
 
-export default Profile
+export default Profile;
