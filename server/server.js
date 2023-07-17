f5ea8d3cdc92fec5848a6150cae261bccec77752
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-07-18 00:30:06 +0200

    issuemodel

--- server/server.js
+++ server/server.js
@@ -7,8 +7,13 @@ const port = process.env.PORT || 6000;
 
 const usersRoute=require("./routes/usersRoute");
 const booksRoute=require("./routes/booksRoute");
+
+
+
 app.use("/api/users",usersRoute);
 app.use("/api/books",booksRoute);
+
+
 app.listen(port, () => {
     console.log(`Hello, World! Server is running on port ${port}`);
 });
