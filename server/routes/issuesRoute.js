f6725117a8ed82b21b7910f32acabb2fd6845b3c
gauthammanjunath <gauthammanjunath007@gmail.com>
2023-07-18 17:47:33 +0200

    routes

---
+++ server/routes/issuesRoute.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const Issue = require("../models/issuesModel");
+const Book = require("../models/booksModel");
+const authMiddleware = require("../middlewares/authMiddleware");
+
+
+router.post("/issue-new-book", authMiddleware, async (req, res) => {
+    try {
+      // inventory adjustment (available copies must be decremented by 1)
+      await Book.findOneAndUpdate(
+        { _id: req.body.book },
+        { $inc: { availableCopies: -1 } }
+      );
+  
+      // issue book to patron (create new issue record)
+      const newIssue = new Issue(req.body);
+      await newIssue.save();
+      return res.send({
+        success: true,
+        message: "Book issued successfully",
+        data: newIssue,
+      }); 
+    } catch (error) {
+      return res.send({
+        success: false,
+        message: error.message,
+      });
+    }
+  });
